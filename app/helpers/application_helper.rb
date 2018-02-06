f2f3479526c47d614574abb4637cce4f5a167ec9
stwebyy <stweb.yy@gmail.com>
2018-02-06 20:28:57 +0900

    Boostrap導入

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # ページタイトルの保管
+  def full_title(page_title)
+    base_title="就活ツール"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
