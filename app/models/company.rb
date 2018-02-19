e0bd7dd44f7a4d506f6f976c9fb90464d9aa213c
fumimi <info@fumimi.jp>
2018-02-19 22:55:37 +0900

    companyコントローラー削除、companiesコントローラー作成

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,5 @@
 class Company < ActiveRecord::Base
-  belongs_to :user
-  validates :user_id, presence: true
-  validates :name, presence: true, length: { maximum: 30 }
+  # belongs_to :user
+  # validates :user_id, presence: true
+  # validates :name, presence: true, length: { maximum: 30 }
 end
